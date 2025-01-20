e0d35d4020800ce365d2e47401f68fbcc9204796
Anyel <anyelmabell@gmail.com>
2025-01-20 12:14:59 -0500

    Copiar datos

--- hboardpage.tsx appsharedschedulepage.tsx
+++
@@ -1,5 +0,0 @@
-[33m362cb33[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mDmain[m[33m, [m[1;31morigin/main[m[33m)[m fix: actualizar credenciales de Supabase para producción
-[33me17a10c[m fix: actualizar configuraciones para producción
-[33me908d04[m fix: corregir errores de tipos y build exitoso
-[33ma36004c[m feat: Implementación de permisos y layouts para superadmin y rutas compartidas
-[33m3a78c92[m[33m ([m[1;31morigin/Dmain[m[33m)[m feat: Implementación de autenticación y autorización para usuarios enterprise y admin con redirección a sus respectivos dashboards
